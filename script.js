afbb690738bf3bbd3c59b20fce524f07c765b9ff
zante00 <zante0@gmail.com>
2023-06-27 15:50:53 -0500

    Add oneGame function to play one game

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 function getComputerChoice() {
     let comThrow = Math.floor(Math.random() * 3);
     if (comThrow == 0) {
-        return "Rock";
+        return "rock";
     } else if (comThrow == 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 function oneGame(playerThrow) {
-    
+    let player = playerThrow.toLowerCase();
+    let com = getComputerChoice();
+
+    console.log (player, com);
+
+    if (player == "rock") {
+        if (com == "rock") {
+            return "Tie";
+        } else if (com == "paper") {
+            return "Lose";
+        } else if (com == "scissors") {
+            return "Win";
+        }
+    } else if (player == "paper") {
+        if (com == "rock") {
+            return "Win";
+        } else if (com == "paper") {
+            return "Tie";
+        } else if (com == "scissors") {
+            return "Lose";
+        }
+    } else if (player == "scissors") {
+        if (com == "rock") {
+            return "Lose";
+        } else if (com == "paper") {
+            return "Win";
+        } else if (com == "scissors") {
+            return "Tie";
+        }
+    }
 }
